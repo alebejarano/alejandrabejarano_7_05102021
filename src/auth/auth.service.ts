c90d290c142464dcca18d4639be7efb26f2aed13
Alejandra Bejarano Alarcon <alejandrabejarano6@gmail.com>
2021-10-13 12:54:25 +0200

    login auth refacto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,47 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable, Logger, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
-  public async validateUser(email: string, plainTextPassword: string) {
-    try {
-      const user = await this.usersService.findByEmail(email);
-      await this.verifyPassword(plainTextPassword, user.password);
-      user.password = undefined;
-      return user;
-    } catch (error) {
-      throw new HttpException(
-        'Wrong credentials provided',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-  }
 
-  private async verifyPassword(
-    plainTextPassword: string,
-    hashedPassword: string,
-  ) {
-    const isPasswordMatching = await bcrypt.compare(
-      plainTextPassword,
-      hashedPassword,
-    );
-    if (!isPasswordMatching) {
-      throw new HttpException(
-        'Wrong credentials provided',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-  }
-
-  /*async validateUser(email: string, password: string): Promise<any> {
-    Logger.log('Holaaa', email);
+  async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
     if (user) {
-      const hash = await bcrypt.hash(password, 10);
-      const isMatch = await bcrypt.compare(user.password, hash);
+      const isMatch = await bcrypt.compare(password, user.password);
       if (isMatch) {
         const { password, ...rest } = user;
         return rest;
@@ -49,21 +18,4 @@ export class AuthService {
     }
     return null;
   }
-}*/
-  /*import { Injectable } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
-
-@Injectable()
-export class AuthService {
-  constructor(private usersService: UsersService) {}
-
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) {
-      const { password, ...result } = user;
-      return result;
-    }
-    return null;
-  }
-}*/
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto, ModifiedUserDto } from './dto/users.dto';
-export type UserTest = any;
 
 @Injectable()
 export class UsersService {
@@ -11,23 +10,6 @@ export class UsersService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
-
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
-
-  async findOne(username: string): Promise<UserTest | undefined> {
-    return this.users.find((user) => user.username === username);
-  }
 
   findAll(): Promise<User[]> {
     return this.usersRepository.find(); //SELECT * from user
