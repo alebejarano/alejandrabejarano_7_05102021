c4f5e990be8b720148b1642556a30efc5268817d
Alejandra Bejarano Alarcon <alejandrabejarano6@gmail.com>
2021-12-11 13:58:30 +0100

    env example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
@@ -19,10 +18,10 @@ export class AppController {
     */
   }
 
-  @UseGuards(JwtAuthGuard)
+  /*@UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
-    //returns the id and email of the connected user
-  }
+    //returns the id and email of the user
+  }*/
 }
