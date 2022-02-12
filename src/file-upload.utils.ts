9e11147157238cddd4fceb379951249969db0a96
Alejandra Bejarano Alarcon <alejandrabejarano6@gmail.com>
2022-02-12 13:34:17 +0100

    fix uploaded filename

--- src/file-upload.utils.ts
+++ src/file-upload.utils.ts
@@ -6,19 +6,19 @@ export const imageFileFilter = (req, file, callback) => {
   callback(null, true);
 };
 
+/*
 const MIME_TYPES = {
   //allows to translate a MIME TYPE into an extension
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png',
   'image/gif': 'gif',
-};
+};*/
 
 export const editFilename = (req, file, callback) => {
   // give the full file name: name + extension
   const name = file.originalname.split(' ').join('_');
   //to deal with the white space and to be replace by _
-  const extension = MIME_TYPES[file.mimetype];
-  callback(null, name + '_' + Date.now() + '.' + extension);
+  callback(null, Date.now() + '_' + name);
   //we add the date to avoid overwritten files
 };
